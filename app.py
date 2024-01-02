0923121de6baf95ee4822341ad3fbb66616426bb
Som-Nang <somnangmega13@gmail.com>
2024-01-02 11:02:00 +0700

    generate image to speech more than 30 word

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
 
 def img2text(url):
-    image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+    image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
     text = image_to_text(url)[0]["generated_text"]
     print("Image to Text:", text)
     return text
 
 def generate_story(scenario):
     template = """ You are a story teller;
-    You can generate a simple story based on a simple narrative, the story should be not more than 20 words;
+    You can generate a simple story based on a simple narrative, the story should be not more than 30 words;
     
     CONTEXT: {scenario}
     STORY:
@@ -31,23 +31,20 @@ def generate_story(scenario):
     story = story_llm.predict(scenario=scenario)
     print("Generated Story:", story)
     return story
-
 def text2speech(message):
-    API_URL = "https://api-inference.huggingface.co/models/microsoft/speecht5_tts"
-    headers = {"Authorization": "Bearer {HUGGINGFACEHUB_API_TOKEN}"}
+    API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+    headers = {"Authorization": f"Bearer {HUGGINGFACEHUB_API_TOKEN}"}
     payloads = {"inputs": message}
     
     response = requests.post(API_URL, headers=headers, json=payloads)
-    
-    with open('audio.flac', 'wb') as file:
-        file.write(response.content)
-    
-#     print("Text to Speech: Audio file generated")
 
-# # Example usage:
-# scenario = img2text("12.jpg")
-# story = generate_story(scenario)
-# text2speech(story)
+    response = requests.post(API_URL, headers=headers, json=payloads)
+    return response.content
+    # print(response.content)  # Add this line to print the response content
+
+    # with open('audio.flac', 'wb') as file:
+    #     file.write(response.content)
+
 def main():
     st.set_page_config(page_title= "img 2 audio story", page_icon="😂")
 
@@ -58,18 +55,17 @@ def main():
         bytes_data = uploaded_file.getvalue()
         with open(uploaded_file.name, "wb") as file:
             file.write(bytes_data)
-        st.image(uploaded_file, caption='Uploaded Img. ', 
-                 use_column_width=True)
+        st.image(uploaded_file, caption='Uploaded Img. ', use_column_width=True)
         scenario = img2text(uploaded_file.name)
         story = generate_story(scenario)
-        text2speech(story)
+        audio_content = text2speech(story)
 
         with st.expander("scenario"):
             st.write(scenario)
         with st.expander("story"):
             st.write(story)
 
-            st.audio("audio.flac")
+            st.audio(audio_content, format="audio/flac")
 
 if __name__ == '__main__':
     main()
